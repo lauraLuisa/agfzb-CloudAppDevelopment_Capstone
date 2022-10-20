8f60eead79a04574c10e95a0c5e05e6df65a42ca
Laura <laura.luisa.schmid@gmail.com>
2022-10-20 08:16:31 -0400

    added models and data

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    description = models.CharField(max_length=500)
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " - " + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +25,30 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    CARTYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon')
+    ]
+
+    car_model = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.CharField(null=False, max_length=100)
+    car_type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=CARTYPE_CHOICES,
+        default=SEDAN
+    )
+    year = models.DateField(null=True)
+
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " - " + self.car_type + " - " + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
