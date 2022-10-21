1843f23ee7aac14a36e52be838f1f4f3efd9d222
Laura <laura.luisa.schmid@gmail.com>
2022-10-21 11:37:32 -0400

    containerization

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "djangoapp-laura.eu-gb.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "laura-luisa.eu-gb.mybluemix.net"]
 
 
 # Application definition
